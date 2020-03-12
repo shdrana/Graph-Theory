fcaa55b5d5cb543c6fd2996b4888d8e01fbb8380
Sohid Ullah <sw-sohid@networld-bd.com>
2020-03-12 12:10:22 +0600

    bfs adjacency added

--- bfs_adjecency_list.cpp
+++ bfs_adjecency_list.cpp
@@ -1,82 +1,84 @@
+//#include <bits/stdc++.h>
 #include <iostream>
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <queue>
+#define MAX_NODE 6
 
 /**
-    Breadth First Search Algorithm using adjacency List
-    @Author Sohid Ullah
-    @Since 2019.08.15
+	Breadth First Search Algorithm using adjacency List
+	@Author Sohid Ullah
+	@Since 2019.08.15
 
-    Total Number of adjacent: 6
-    Time Complexity: O(V+E)
+	Total Number of adjacent: 6
+	Time Complexity: O(V+E)
 
 **/
 
 using namespace std;
 
-void bfsTraversal(vector<int > g[], int numberOfNode, int source)
+void bfsTraversal(vector<int > graph[], int numberOfNode, int source)
 {
-    int i, visited[numberOfNode], v, level[numberOfNode];
+	int i, visited[MAX_NODE], v, level[MAX_NODE];
 
-    for(i = 0; i<numberOfNode; i++)
-    {
-        visited[i] = 0;
-    }
+	for (i = 0; i < numberOfNode; i++)
+	{
+		visited[i] = 0;
+	}
 
-    queue<int> q;
-    q.push(source);
+	queue<int> bfsQueue;
+	bfsQueue.push(source);
+	visited[source] = 1;
+	level[source] = 0;
 
-    visited[source] = 1;
-    level[source] = 0;
-
-    while(!q.empty())
-    {
-        int u = q.front();
-        q.pop();
-        printf("%d ", u);
-        for(v = 0; v<g[u].size(); v++)
-        {
-            int adjecentNodeofU = g[u][v];
-            if(visited[adjecentNodeofU] == 0)
-            {
-                visited[adjecentNodeofU] = 1;
-                level[adjecentNodeofU] = level[u]+1;
-                q.push(adjecentNodeofU);
-            }
-        }
-        cout<<endl;
-    }
+	while (!bfsQueue.empty())
+	{
+		int u = bfsQueue.front();
+		bfsQueue.pop();
+		printf("%d ", u);
+		for (v = 0; v < graph[u].size(); v++)
+		{
+			int adjecentNodeofU = graph[u][v];
+			if (visited[adjecentNodeofU] == 0)
+			{
+				visited[adjecentNodeofU] = 1;
+				level[adjecentNodeofU] = level[u] + 1;
+				bfsQueue.push(adjecentNodeofU);
+			}
+		}
+	}
 }
 
 
 int main()
 {
-    vector<int > v[6];
+	vector<int > graphStorage[MAX_NODE];
 
-    v[0].push_back(1);
-    v[0].push_back(2);
-    v[0].push_back(3);
+	graphStorage[0].push_back(1);
+	graphStorage[0].push_back(2);
+	graphStorage[0].push_back(3);
 
-    v[1].push_back(0);
-    v[1].push_back(2);
+	graphStorage[1].push_back(0);
+	graphStorage[1].push_back(2);
 
-    v[2].push_back(0);
-    v[2].push_back(1);
-    v[2].push_back(3);
-    v[2].push_back(5);
+	graphStorage[2].push_back(0);
+	graphStorage[2].push_back(1);
+	graphStorage[2].push_back(3);
+	graphStorage[2].push_back(5);
 
-    v[3].push_back(0);
-    v[3].push_back(2);
-    v[3].push_back(5);
-    v[3].push_back(4);
+	graphStorage[3].push_back(0);
+	graphStorage[3].push_back(2);
+	graphStorage[3].push_back(5);
+	graphStorage[3].push_back(4);
 
-    v[4].push_back(3);
-    v[4].push_back(5);
+	graphStorage[4].push_back(3);
+	graphStorage[4].push_back(5);
 
-    v[5].push_back(4);
-    v[5].push_back(3);
-    v[5].push_back(2);
+	graphStorage[5].push_back(4);
+	graphStorage[5].push_back(3);
+	graphStorage[5].push_back(2);
 
-    bfsTraversal(v, 6, 0);
+	bfsTraversal(graphStorage, 6, 0);
 
-    return 0;
+	return 0;
 }
